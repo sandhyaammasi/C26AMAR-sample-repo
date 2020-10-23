12c2ef1a20b739a4892281e4d39313899e2bcec5
Sandhya Ammasi <sandhyaammasi@gmail.com>
2020-10-23 06:45:24 +0530

    created a pig object

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    pig4 = new Pig(400,400);
     bird = new Bird(100,100);
 
 }
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    pig4.display();
     bird.display();
     platform.display();
 }
